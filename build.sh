835ea3ec31e1396c6f4e0f646f091a4c73ab7155
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-27 11:43:44 +0100

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
+#!/bin/bash
 docker build --no-cache -t hackinglab/alpine-ssh-mitm:3.2.0 -t hackinglab/alpine-ssh-mitm:3.2 -t hackinglab/alpine-ssh-mitm:latest -f Dockerfile .
 
-docker push hackinglab/alpine-ssh-mitm
-docker push hackinglab/alpine-ssh-mitm:3.2
-docker push hackinglab/alpine-ssh-mitm:3.2.0
